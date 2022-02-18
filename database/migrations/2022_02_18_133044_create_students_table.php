8a26cada29486bb3b21bba470781a90cc9eb1214
geoffrey <geoffreygosset123@gmail.com>
2022-02-18 14:51:21 +0100

    DB ok table create

---
+++ database/migrations/2022_02_18_133044_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('firstname', 40);
+            $table->string('email', 50)->nullable();
+            $table->string('adresse', 200);
+            $table->date('naissance');
+            $table->ipAddress('adresseIP');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
